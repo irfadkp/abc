0ee3f6ed91f39235b99a1336f63e7f46e939a767
irfadkp <54243898+irfadkp@users.noreply.github.com>
2019-10-29 19:48:46 +0530

    updated

--- add.c
+++ add.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
  void main()
 { int a,b;
-printf("enter the numbers");
-scanf("  %d %d",&a,&b);
+printf("enter the first number");
+scanf("%d",&a);
+ printf("enter the second number");
+ scanf("%d",&b);
 printf(" the sum is");
 printf(" %d",a+b);
 
